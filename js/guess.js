f1c016d63da83ba3c394f0946f272bc9e0e30118
EOengineer <EOengineer@gmail.com>
2014-05-15 17:47:01 -0400

    basic logic written, need to troubleshoot

---
+++ js/guess.js
@@ -0,0 +1,7 @@
+var answer = Math.floor((Math.random() * 10) + 1);
+var guess = prompt("Guess a number between 1 and 10");
+if (guess() === answer) {
+  alert(guess + ' is the answer! Good Job! You Won!!');
+} else {
+  alert(guess + 'is the wrong answer.  Bummer, you lose.');
+}
