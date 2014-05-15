f15312d59a321767ce65ca12f45d63da60891ff9
EOengineer <EOengineer@gmail.com>
2014-05-15 17:57:22 -0400

    now working. Will add looping shortly.

--- js/guess.js
+++ js/guess.js
@@ -1,7 +1,7 @@
-var answer = Math.floor((Math.random() * 10) + 1);
+var answer = Math.floor((Math.random() * 10) + 1).toString();
 var guess = prompt("Guess a number between 1 and 10");
-if (guess() === answer) {
+if (guess === answer) {
   alert(guess + ' is the answer! Good Job! You Won!!');
 } else {
-  alert(guess + 'is the wrong answer.  Bummer, you lose.');
+  alert(guess + ' is the wrong answer.  Bummer, you lose.');
 }
