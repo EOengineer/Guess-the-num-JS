271052c58b9da9c6e3924aab1c8bc2eefeebe05e
EOengineer <EOengineer@gmail.com>
2014-05-15 18:46:04 -0400

    added looping for input request

--- js/guess.js
+++ js/guess.js
@@ -1,7 +1,10 @@
-var answer = Math.floor((Math.random() * 10) + 1).toString();
+var guess = 0;
+var answer = Math.floor((Math.random() * 2) + 1).toString();
+while (guess !== answer) {
 var guess = prompt("Guess a number between 1 and 10");
 if (guess === answer) {
   alert(guess + ' is the answer! Good Job! You Won!!');
 } else {
-  alert(guess + ' is the wrong answer.  Bummer, you lose.');
+  alert(guess + ' is not it! Guess again!');
+}
 }
